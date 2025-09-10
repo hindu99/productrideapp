39e7687468d39f8a31e0a3d0c478b7456c22bab6
hindu99 <hindu.v@gmail.com>
2025-09-10 08:54:33 +0100

    Jest framework installed for backend testing

---
+++ backend/tests/testhelper.js
@@ -0,0 +1,10 @@
+
+export function mockResponse() {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+}
+export function mockRequest(body = {}) {
+  return { body };
+}
